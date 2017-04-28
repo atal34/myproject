e9942c49d5aa4b875a5149a38536208b1dcebf5e
ATAL <ATAL@ATAL-PC>
2017-04-28 21:25:35 +0530

    test exception

---
+++ new/src/TestException.java
@@ -0,0 +1,15 @@
+
+public class TestException {
+	
+	public static void display(String msg){
+		System.out.println(msg);
+		
+	}
+
+	public static void main(String args){
+		
+		display("new exception message");
+		
+		
+	}
+}
