c2c24ed9469cb24c18f81411c6f6ebfe7dc74241
689218 <689218@CTSC01248431001.cts.com>
2019-05-16 12:21:00 +0530

    Outlook mail sample

--- new/src/email/SendMailSSL.java
+++ new/src/email/SendMailSSL.java
@@ -13,26 +13,34 @@ import javax.mail.internet.MimeMessage;
 public class SendMailSSL {
 	public static void main(String[] args) {
 		Properties props = new Properties();
-		props.put("mail.smtp.host", "smtp.gmail.com");
-		props.put("mail.smtp.socketFactory.port", "465");
+		//props.put("mail.smtp.host", "smtp.gmail.com");
+		//props.put("mail.smtp.socketFactory.port", "465");
+		//props.put("mail.smtp.socketFactory.class",
+				//"javax.net.ssl.SSLSocketFactory");
+		//props.put("mail.smtp.auth", "true");
+		//props.put("mail.smtp.port", "465");
+
+		
+		props.put("mail.smtp.host", "smtp.cognizant.com");
+		props.put("mail.smtp.socketFactory.port", "25");
 		props.put("mail.smtp.socketFactory.class",
 				"javax.net.ssl.SSLSocketFactory");
 		props.put("mail.smtp.auth", "true");
-		props.put("mail.smtp.port", "465");
-
+		props.put("mail.smtp.port", "25");
+		
 		Session session = Session.getDefaultInstance(props,
 			new javax.mail.Authenticator() {
 				protected PasswordAuthentication getPasswordAuthentication() {
-					return new PasswordAuthentication("rkresidencysociety@gmail.com","rksociety");
+					return new PasswordAuthentication("atalphatak@cognizant.com","Asdqwe$344");
 				}
 			});
 
 		try {
 
 			Message message = new MimeMessage(session);
-			message.setFrom(new InternetAddress("rkresidencysociety@gmail.com"));
+			message.setFrom(new InternetAddress("atal34@gmail.com"));
 			message.setRecipients(Message.RecipientType.TO,
-					InternetAddress.parse("atal34@gmail.com"));
+					InternetAddress.parse("amitjaswal4u@gmail.com"));
 			message.setSubject("Testing Subject");
 			message.setText("Dear Mail Crawler," +
 					"\n\n No spam to my email, please!");
